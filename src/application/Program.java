1050082caf2c08337e98f76e22f9a69f877d9387
adriano araujo <adriano.amedeiros@hotmail.com>
2020-04-06 20:12:06 -0300

    Implementação das classes Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Position p = new Position(3,5);
-		System.out.println(p);
+		Board b = new Board(3,5);
+		System.out.println(b);
 	}
 
 }
